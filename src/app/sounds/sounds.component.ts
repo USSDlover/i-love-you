be64b38f821bf75995ba7e3d5c19866b2db7078d
Alireza Razinejad <a.razinejad@gmail.com>
2021-03-22 13:27:32 +0430

    Make sounds play optionally v1.5.4

--- src/app/sounds/sounds.component.ts
+++ src/app/sounds/sounds.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, EventEmitter, OnDestroy, ViewChild, ElementRef} from '@angular/core';
-import {Subscription} from 'rxjs';
+import {interval, of, Subscription} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-sounds',
@@ -9,8 +10,12 @@ import {Subscription} from 'rxjs';
 export class SoundsComponent implements OnInit, OnDestroy {
   @Input() sayILoveYou = new EventEmitter();
   @ViewChild('iLoveYou', {static: true}) iLoveYou: ElementRef;
+  @ViewChild('relaxing', {static: true}) relaxing: ElementRef;
+  @ViewChild('heartBeat', {static: true}) heartBeat: ElementRef;
 
   private iLoveYouSub: Subscription;
+
+  played: boolean;
 
   constructor() { }
 
@@ -21,10 +26,48 @@ export class SoundsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.sayILoveYou
+    this.onSayILoveYou();
+  }
+
+  onPlaySounds(): void {
+    if (!this.played) {
+      this.relaxing.nativeElement.play();
+      this.heartBeat.nativeElement.play();
+    } else {
+      this.relaxing.nativeElement.pause();
+      this.heartBeat.nativeElement.pause();
+    }
+
+    this.played = !this.played;
+  }
+
+  onSaidILoveYou(): void {
+    this.relaxing.nativeElement.volume = .8;
+    this.heartBeat.nativeElement.volume = .8;
+  }
+
+  private onSayILoveYou(): void {
+    this.iLoveYouSub = this.sayILoveYou
       .subscribe(() => {
+        this.relaxing.nativeElement.volume = .2;
+        this.heartBeat.nativeElement.volume = .2;
         this.iLoveYou.nativeElement.play();
-        console.log('say i love you');
+      });
+  }
+
+  // FIXME
+  private changeVolumeSmoothly(from: number, to: number): void {
+    let currVal = from;
+    interval(450)
+      .pipe(takeUntil(of(currVal = to)))
+      .subscribe(() => {
+        if (from < to) {
+          currVal += 0.1;
+        } else {
+          currVal -= 0.1;
+        }
+        this.relaxing.nativeElement.volume = currVal;
+        this.heartBeat.nativeElement.volume = currVal;
       });
   }
 
